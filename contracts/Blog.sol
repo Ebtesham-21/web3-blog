dc7c43bb8d8afceeca30ca3d90c0a176abd5bf6e
unknown <ebteshamhossain98@gmail.com>
2022-04-19 20:31:10 +0600

    first commit

---
+++ contracts/Blog.sol
@@ -0,0 +1,23 @@
+// contracts/Blow.sol
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Blog {
+    string public name;
+    address public owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _postIds;
+
+    struct Post {
+        uint id;
+        string title;
+        string content;
+        bool published;
+    }
+    /* mappings can be seen as hash tables */
+    /* here we create lookups for posts by id and posts by ipfs hash */
+}

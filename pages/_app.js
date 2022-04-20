b382dbf32381380ebf6e434ba54d5faf49446157
unknown <ebteshamhossain98@gmail.com>
2022-04-21 02:26:51 +0600

    second day

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
+/* pages/_app.js */
 import '../styles/globals.css'
+import { useState } from 'react'
+import Link from 'next/link'
+import {css} from '@emotion/css'
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { AccountContext } from '../context.js'
+import {ownerAddress} from '../config'
+import 'easymde/dist/easymde.min.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}) {
+  /* create local state to save account information after signin */
+  const [account, setAccount] = useState(null)
+  /* web3Modal configuration for enabling wallet access */
+  async function getWeb3Modal(){
+    const web3Modal = new Web3Modal({
+      cacheProvider: false,
+      providerOptions:{
+        walletConnect:{
+          package: WalletConnectProvider,
+          options:{
+            infuraId: "your-infura-id"
+          },
+        },
+
+      },
+    })
+    return web3Modal
+  }
+
+  /* the connect function uses web3 modal to connect to the user's wallet */
+  
 }
-
-export default MyApp
